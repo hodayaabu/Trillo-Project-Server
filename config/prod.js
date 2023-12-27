241a81e05acf17675c5deb81bacf6d22490fd82e
MeitarGiladi <95337588+MeitarGiladi@users.noreply.github.com>
2023-12-27 18:31:14 +0200

    MongoDB works

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName: process.env.DB_NAME || 'tester_db'
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://trelloCrawMongoDb:Gnyv0nFjFOiTM4If@trelloca-cluster.r6rwhb1.mongodb.net',
+  dbName: process.env.DB_NAME || 'TrelloDB'
 }
